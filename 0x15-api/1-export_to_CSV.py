299c8af143db6a5adf3fc60e38aeefc0fb1a1625
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-19 21:44:31 +0100

    task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""a Python script that, using jsonplaceholder.typicode REST API,
-for a given employee ID, returns information about his/her
-TODO list progress"""
+""" a Python script that, for a given employee ID,
+returns information about his/her TODO list progress."""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    emp_id = sys.argv[1]
-    emp_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                            .format(emp_id)).json().get("name")
-    Num_of_task = 0
-    tasks_done = []
+    user = "https://jsonplaceholder.typicode.com/users/"
+    todos = "https://jsonplaceholder.typicode.com/todos"
+    response_user = requests.get(user + "{}".format(sys.argv[1])).json()
+    response_todo = requests.get(todos, params={"userId": sys.argv[1]}).json()
+
+    task_status = []
+    task_title = []
 
     request = requests.get('https://jsonplaceholder.typicode.com//todos')\
         .json()
 
     for test in request:
-        if test.get('userId') == int(emp_id):
-            Num_of_task += 1
-            if test.get('completed') is True:
-                tasks_done.append(test.get('title'))
-
-    print('Employee {} is done with tasks({}/{}):'
-          .format(emp_name, len(tasks_done), Num_of_task))
-    for task_title in tasks_done:
-        print('\t {}'.format(task_title))
+        if test.get('userId') == int(sys.argv[1]):
+            task_status.append(test.get('completed'))
+            task_title.append(test.get('title'))
+    with open("{}.csv".format(sys.argv[1]), "a") as f:
+        for status, title in zip(task_status, task_title):
+            print('"{}","{}","{}","{}"'
+                  .format(sys.argv[1], response_user, status, title), file=f)
