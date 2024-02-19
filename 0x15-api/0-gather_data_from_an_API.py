5774c30ae0246ebda7f9e9d2b1eaba32c2584c5c
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-19 21:28:09 +0100

    task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""a Python script that returns information about his/her TODO list progress."""
-
+""" a Python script that, for a given employee ID,
+returns information about his/her TODO list progress."""
 import requests
 import sys
 
-def employee_todo(employee_id):
-    employee_resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    employee_data = employee_resp.json()
-    employee_name = employee_data['name']
 
-    todo_resp = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-    todo_data = todo_resp.json()
+if __name__ == "__main__":
+    user = "https://jsonplaceholder.typicode.com/users/"
+    todos = "https://jsonplaceholder.typicode.com/todos"
+    response_user = requests.get(user + "{}".format(sys.argv[1])).json()
+    response_todo = requests.get(todos, params={"userId": sys.argv[1]}).json()
+    """keeps track of num of completed tasks"""
+    counter = 0
+    """stores num of the users tasks"""
+    len_tasks = len(response_todo)
 
-    completed_tasks = [task for task in todo_data if task['completed']]
-    number_compl_tasks = len(completed_tasks)
-    total_num_tasks = len (todo_data)
-
-    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
-
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
-
-    if __name__ == "__main__":
-        employee_id = int(sys.argv[1])
-        employee_todo(employee_id)
+    name = response_user.get("name")
+    for task in response_todo:
+        if task.get("completed"):
+            counter += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, counter, len_tasks))
+    for task in response_todo:
+        if task.get("completed"):
+            print("\t {}".format(task.get("title")))
