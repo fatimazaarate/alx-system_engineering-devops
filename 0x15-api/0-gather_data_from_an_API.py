7522fd73cd253e562b3c21ab46d6e9385f8cb274
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-19 21:40:01 +0100

    task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-""" a Python script that, for a given employee ID,
-returns information about his/her TODO list progress."""
+"""a Python script that, using jsonplaceholder.typicode REST API,
+for a given employee ID, returns information about his/her
+TODO list progress and  export data in the CSV format."""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    user = "https://jsonplaceholder.typicode.com/users/"
-    todos = "https://jsonplaceholder.typicode.com/todos"
-    response_user = requests.get(user + "{}".format(sys.argv[1])).json()
-    response_todo = requests.get(todos, params={"userId": sys.argv[1]}).json()
-    """keeps track of num of completed tasks"""
-    counter = 0
-    """stores num of the users tasks"""
-    len_tasks = len(response_todo)
+    emp_id = sys.argv[1]
+    user_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(emp_id)).json().get("username")
+    task_status = []
+    task_title = []
 
-    name = response_user.get("name")
-    for task in response_todo:
-        if task.get("completed"):
-            counter += 1
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, counter, len_tasks))
-    for task in response_todo:
-        if task.get("completed"):
-            print("\t {}".format(task.get("title")))
+    request = requests.get('https://jsonplaceholder.typicode.com//todos')\
+        .json()
+
+    for test in request:
+        if test.get('userId') == int(emp_id):
+            task_status.append(test.get('completed'))
+            task_title.append(test.get('title'))
+    with open("{}.csv".format(emp_id), "a") as f:
+        for status, title in zip(task_status, task_title):
+            print('"{}","{}","{}","{}"'
+                  .format(emp_id, user_name, status, title), file=f)
