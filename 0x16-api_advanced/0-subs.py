4d672b8a8cba9a0921a627552c6490f7ad177432
fatimazaarate <fatima.zaarate@gmail.com>
2024-04-09 16:08:32 +0000

    task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """ a function that queries the Reddit API and returns the number
+    of subscribers for a given subreddit """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user_header = {'User-Agent': 'custom User-Agent'}
+
+    response = requests.get(url, headers=user_header, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers_number = data['data']['subscribers']
+        return subscribers_number
+    else:
+        return 0
