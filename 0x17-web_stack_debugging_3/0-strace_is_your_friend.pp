086819e41fbcda9c10c19470b86557a6aa9dff27
fatimazaarate <fatima.zaarate@gmail.com>
2024-04-11 17:41:40 +0000

    web

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# this is a script that fixes why apache returns a 500 error# this is a script that fixes why apache returns a 500 error
+
+exec { 'fix-error':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
+}
